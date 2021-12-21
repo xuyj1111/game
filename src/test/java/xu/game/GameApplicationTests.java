3008a328e335031edea601eb3fe1da8a431f5893
yujun.xu <yujun.xu@linkflowtech.com>
2021-12-21 15:52:53 +0800

    Basic environment

---
+++ src/test/java/xu/game/GameApplicationTests.java
@@ -0,0 +1,13 @@
+package xu.game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
