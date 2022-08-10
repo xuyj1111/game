b79684617f511d5dd4fbc5c6fae5ae3282854baf
yujun.xu <yujun.xu@linkflowtech.com>
2022-08-10 11:25:30 +0800

    小修改

--- okay/src/main/resources/script/init.sql
+++ okay/src/main/resources/script/init.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS `user` (
     `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(255) NOT NULL COMMENT '用户名',
     `pwd` VARCHAR(255) NOT NULL COMMENT '密码',
-    `record` INT(11) DEFAULT 1 COMMENT '闯关纪录',
+    `record` INT(11) NOT NULL DEFAULT 1 COMMENT '闯关纪录',
     PRIMARY KEY (`id`),
     UNIQUE KEY `un_name` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -17,8 +17,8 @@ CREATE TABLE IF NOT EXISTS `level` (
 	`level_id` BIGINT(20) NOT NULL COMMENT '关卡序号',
     `name` VARCHAR(255) NOT NULL COMMENT '关卡名',
     `map` LONGTEXT NOT NULL COMMENT '地图',
-	`user` VARCHAR(255) DEFAULT NULL COMMENT '使用者',
-	`is_system` TINYINT(1) DEFAULT 1 COMMENT '是否系统，0否1是',
+	`user` VARCHAR(255) NOT NULL COMMENT '使用者',
+	`is_system` TINYINT(1) NOT NULL COMMENT '是否系统，0否1是',
     PRIMARY KEY (`id`),
     UNIQUE KEY `uni_idx_levelId_user_system` (`level_id`,`user`,`is_system`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
